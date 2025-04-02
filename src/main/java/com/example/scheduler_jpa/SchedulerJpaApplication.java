b1e9b8165305e5474da7e8da6f3c81e2bc6718ef
dikaprio1 <159988070+dikaprio1@users.noreply.github.com>
2025-04-02 14:34:48 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/scheduler_jpa/SchedulerJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.scheduler_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerJpaApplication.class, args);
+    }
+
+}
